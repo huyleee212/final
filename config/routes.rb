32e75e6ff9884a7a2daeb31e10fae4062ceed1e6
huyleee212 <0906993478.vh@gmail.com>
2025-01-10 13:17:17 +0700

    ok

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  resources :products
+
+  get "/products", to: "products#index"
+
+get "/products/new", to: "products#new"
+post "/products", to: "products#create"
+
+get "/products/:id", to: "products#show"
+
+get "/products/:id/edit", to: "products#edit"
+patch "/products/:id", to: "products#update"
+put "/products/:id", to: "products#update"
+
+delete "/products/:id", to: "products#destroy"
+root "products#index"
+
+end
